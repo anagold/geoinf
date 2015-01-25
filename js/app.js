21a34f1f458c00fa295b99681dd94e35180103da
anagold <anna.golonka.91@gmail.com>
2015-01-25 22:22:34 +0000

    Firebase

--- js/app.js
+++ js/app.js
@@ -1,9 +1,49 @@
-angular.module('myApp', [])
-    .controller('myCrl', function($scope, $http) {
-        var url = 'https://gdata.youtube.com/feeds/api/users/orbitalofficial/uploads?alt=json-in-script&callback=JSON_CALLBACK' 
+var app = angular.module('myApp', ['firebase'])
+
+app.constant('FIREBASE_URI', 'https://anagold.firebaseio.com/')
+app.controller('myCtrl', function($scope, ItemsService){
+    $scope.items = ItemsService.getItems();
+    $scope.isUpdated = false;
+    
+    $scope.addItem = function(text) {
+        ItemsService.addItem({text: text});
+    };
+    $scope.removeItem = function (id) {
+        ItemsService.removeItem(id);
+    };
+    $scope.updateItem = function (id) {
+        ItemsService.upadeItem(id);
+    };
+})
+ 
+app.factory('ItemsService', function($firebase, FIREBASE_URI) {
+    var ref = new Firebase(FIREBASE_URI + "/items/");
+    var sync = $firebase(ref);
+    var items = sync.$asArray();
+    
+    var getItems = function() {
+        return items;
+    };
+    
+    var addItem = function(item) {
+        items.$add(item);
+    };
+    
+    var removeItem = function(id) {
+        items.$remove(id);
+    };
+    
+    var updateItem = function(id) {
+        items.$save(id);
+    }
+    
+    return {
+        getItems: getItems,
+        addItem: addItem,
+        removeItem: removeItem,
+        upadeItem: updateItem
+    }
+})
+
+
         
-        $http.jsonp(url)
-            .success(function(data) {
-                $scope.results = data.feed.entry;   
-            });
-    });
